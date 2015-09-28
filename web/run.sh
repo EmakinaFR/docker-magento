66b404b6d74d10ab966e2c78dd3491f634877ea1
Alexandre Jardin <info@ajardin.fr>
2015-09-28 14:26:28 +0200

    Add Blackfire container

--- web/run.sh
+++ web/run.sh
@@ -3,6 +3,7 @@
 function init_server()
 {
     APP_IP="$(/sbin/ifconfig eth0| grep "inet addr:" | awk {"print $2"} | cut -d ":" -f 2)"
+    echo "ServerName localhost" >> /etc/apache2/apache2.conf
 
     service zend-server start
     WEB_API_KEY="$(cut -s -f 1 /root/api_key 2> /dev/null)"
@@ -15,8 +16,6 @@ function init_server()
         WEB_API_KEY="$(cut -s -f 1 /root/api_key)"
         WEB_API_KEY_HASH="$(cut -s -f 2 /root/api_key)"
     fi
-
-    "${ZS_MANAGE}" restart -N "${WEB_API_KEY}" -K "${WEB_API_KEY_HASH}"
 }
 
 function init_configuration()
@@ -27,8 +26,9 @@ function init_configuration()
     mv /usr/local/zend/etc/sites.d/zend-default-vhost-80.conf /usr/local/zend/etc/sites.d/zend-default-vhost-8080.conf
     mv /usr/local/zend/etc/sites.d/http/__default__/80 /usr/local/zend/etc/sites.d/http/__default__/8080
 
-    "${ZS_MANAGE}" store-directive -d zray.enable -v 0 -N "${WEB_API_KEY}" -K "${WEB_API_KEY_HASH}"
     "${ZS_MANAGE}" extension-on -e mongo -N "${WEB_API_KEY}" -K "${WEB_API_KEY_HASH}"
+    "${ZS_MANAGE}" extension-off -e "Zend OPcache" -N "${WEB_API_KEY}" -K "${WEB_API_KEY_HASH}"
+    "${ZS_MANAGE}" store-directive -d zray.enable -v 0 -N "${WEB_API_KEY}" -K "${WEB_API_KEY_HASH}"
 }
 
 function init_vhosts()
@@ -40,8 +40,6 @@ function init_vhosts()
     if [[ -d "${VHOSTS_PATH}" ]]; then
         VHOST_FILES="$(find "${VHOSTS_PATH}" -maxdepth 1 -type f -name *:*)"
         if [[ ! -z "${VHOST_FILES}" ]]; then
-            NEW_VHOSTS=""
-
             for FILE in ${VHOST_FILES}; do
                 FILENAME="$(basename "${FILE}")"
 
@@ -49,20 +47,26 @@ function init_vhosts()
                 VHOST_PORT="$(echo "${FILENAME}" | cut -d : -f 2)"
                 VHOST_CONTENT="$(< "${FILE}")"
 
-                CREATION_OUTPUT="$("${ZS_MANAGE}" vhost-add -n "${VHOST_NAME}" -p "${VHOST_PORT}" \
-                    -t "$VHOST_CONTENT" -N "${WEB_API_KEY}" -K "${WEB_API_KEY_HASH}" 2>&1)"
-
-                VHOST_ID="$(echo "${CREATION_OUTPUT}" | cut -d " " -f 2)"
-                if [[ "${VHOST_ID}" =~ ^[0-9]+$ ]]; then
-                    NEW_VHOSTS="${NEW_VHOSTS} ${VHOST_NAME}"
-                fi
+                "${ZS_MANAGE}" vhost-add -n "${VHOST_NAME}" -p "${VHOST_PORT}" \
+                    -t "$VHOST_CONTENT" -N "${WEB_API_KEY}" -K "${WEB_API_KEY_HASH}" 2>&1
             done
-
-            if [[ -n "${NEW_VHOSTS}" ]]; then
-                echo "127.0.0.1 ${NEW_VHOSTS}" >> /etc/hosts
-            fi
         fi
     fi
+}
+
+function init_blackfire()
+{
+    read -r -d '' BLACKFIRE_INI <<HEREDOC
+extension=blackfire.so
+blackfire.agent_socket=tcp://blackfire:${BLACKFIRE_PORT}
+blackfire.agent_timeout=5
+blackfire.log_file=/var/log/blackfire.log
+blackfire.log_level=${BLACKFIRE_LOG_LEVEL}
+blackfire.server_id=${BLACKFIRE_SERVER_ID}
+blackfire.server_token=${BLACKFIRE_SERVER_TOKEN}
+HEREDOC
+
+    echo "${BLACKFIRE_INI}" >> /usr/local/zend/etc/conf.d/blackfire.ini
 }
 
 LOCK_FILE="/var/docker.lock"
@@ -72,8 +76,11 @@ if [[ ! -e "${LOCK_FILE}" ]]; then
     init_server
     init_configuration
     init_vhosts
-
     "${ZS_MANAGE}" restart -N "${WEB_API_KEY}" -K "${WEB_API_KEY_HASH}"
+
+    init_blackfire
+    service zend-server restart
+
     touch "${LOCK_FILE}"
 else
     service zend-server start
